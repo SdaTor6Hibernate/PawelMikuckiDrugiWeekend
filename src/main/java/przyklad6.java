c645c8d88fb2ef3753a7101923b69f670bed46e6
Pawel Mikucki <pawel_mikucki@wp.pl>
2020-04-04 12:35:04 +0200

    invalid path

--- src/main/java/przyklad6.java
+++ src/main/java/przyklad6.java
@@ -1,10 +1,13 @@
 import model.Address;
+import model.Country;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import util.HibernateUtil;
 
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -15,9 +18,11 @@ public class przyklad6 {
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<Address> query = builder.createQuery(Address.class);
         Root<Address> addressRoot = query.from(Address.class);
-        query.select(addressRoot);
+        Join<Address, Country> country = addressRoot.join("country");
+        query.select(addressRoot).where(builder.like(country.get("alias"),"PL"));
 
-        Query<Address> q = session.createQuery(query);
+
+        TypedQuery<Address> q = session.createQuery(query);
         List<Address> addressList = q.getResultList();
         addressList.forEach(address -> System.out.println(address.getCity()));
         session.close();
